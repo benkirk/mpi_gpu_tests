9999cc3c6e79e55f90df17195b1b701811cf0833
Ben Kirk <benkirk@ucar.edu>
2022-11-29 18:12:04 -0700

    Casper works, need to unset CUDA_VISIBLE_DEVICES

--- basic_cuda/run_casper.sh
+++ basic_cuda/run_casper.sh
@@ -3,14 +3,17 @@
 #PBS -A SCSG0001
 #PBS -j oe
 #PBS -l walltime=00:30:00
-#PBS -l select=2:ncpus=4:ngpus=2:mem=40G
+#PBS -l select=2:ncpus=2:ngpus=2:mem=40G
 #PBS -l gpu_type=v100
 
 module reset >/dev/null 2>&1
 module load nvhpc >/dev/null 2>&1
 module list
 
-export MPI_ARGS=""
+# use MPI local rank internally to set the CUDA device, not anything from the environment
+unset CUDA_VISIBLE_DEVICES
+
+unset MPI_ARGS="-n 4"
 
 make --no-print-directory clean
 make --no-print-directory
